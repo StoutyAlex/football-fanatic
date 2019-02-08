463a087445ae550309be831e2d64f82a9fd26fb5
Alex Stout <alexstout009@hotmail.com>
2019-02-08 15:33:09 +0000

    Removed unwanted title

--- app/App.js
+++ app/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-// import './App.css'; // Convert to sass
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, World! Football</h1>
+        <h1>Hello, World!</h1>
       </div>
     );
   }
